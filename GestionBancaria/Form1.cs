6c94a1a1891d85a7853e272caa9e74f438d5f0a7
pedro12pv <pedropuerta12@gmail.com>
2022-12-13 20:43:20 +0100

    Errores corregidos

--- GestionBancaria/Form1.cs
+++ GestionBancaria/Form1.cs
@@ -2,29 +2,27 @@ namespace GestionBancaria
 {
     public partial class Form1 : Form
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+        private double saldo_Ppv_2023 = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldo_Ppv_2023.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad)
+        private void realizarReintegro(double cantidad)
         {
-            if (cantidad > 0 && saldo > cantidad)
+            if (cantidad > 0 && saldo_Ppv_2023 >= cantidad)
             {
-                saldo -= cantidad;
-                return true;
+                saldo_Ppv_2023 -= cantidad;
             }
-            return false;
         }
 
         private void realizarIngreso(double cantidad)
         {
-            if (cantidad > 0)
-                saldo += cantidad;
+            if (cantidad >= 0)
+                saldo_Ppv_2023 += cantidad;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
@@ -32,16 +30,26 @@ namespace GestionBancaria
             double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
             if (cantidad < 0)
             {
-                MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
-            }
-            if (rbReintegro.Checked)
-            {
-                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
-                    MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+                MessageBox.Show("Cantidad no válida, sólo se admiten cantidades positivas.");
             }
             else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
+            {
+                if (rbReintegro.Checked)
+                {
+                    realizarReintegro(cantidad);
+                    txtSaldo.Text = saldo_Ppv_2023.ToString();
+                }// No se ha podido completar la operación, saldo insuficiente?
+                    
+
+                if (rbIngreso.Checked)
+                {
+                    realizarIngreso(cantidad);
+                    txtSaldo.Text = saldo_Ppv_2023.ToString();
+                }
+            }
+            if (double.Parse(txtSaldo.Text) < cantidad)
+                MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
         }
+       
     }
 }
